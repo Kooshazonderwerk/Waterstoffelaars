8510829e631f2aae0ff3504ea6738f15284945b1
Raymond-Blok <raymond-blok@outlook.com>
2021-04-22 13:28:08 +0200

    removed commended code

--- GuiPages.py
+++ GuiPages.py
@@ -21,65 +21,6 @@ class StartPage(tk.Frame):
 
         self.roomTabs = ttk.Notebook(self)
         self.loadRooms()
-        # roomFrame = ttk.Frame(self.roomTabs)
-        # self.roomTabs.add(roomFrame, text="room 1")
-        # self.roomTabs.grid(row=1, column=0, sticky="nsew")
-
-        # #legend Frame
-        # frmLegend = ttk.Frame(roomFrame)
-        # lblLegend = ttk.Label(frmLegend, text="Legend")
-
-        # lblLegend.grid(row=0, column=0, padx=5, pady=5)
-
-        # frmLegend.grid(row=0, column=0, padx=5, pady=5)
-        # #Room info frame
-        # frmRoomInfo = ttk.Frame(roomFrame)
-        # lblRoomInfoName = ttk.Label(frmRoomInfo, text="room 1")
-
-        # lblRoomInfoName.grid(row=0, column=0, padx=5, pady=5)
-
-        # frmRoomInfo.grid(row=0, column=1, padx=5, pady=5)
-
-        # #Sensor add button
-        # frmSensorAdd = ttk.Frame(roomFrame)
-        # btnAddSensor = ttk.Button(frmSensorAdd, text="Add Sensor", command=lambda: controller.show_frame(EditSensorPage))
-        # btnAddSensor.pack(side=tk.LEFT)
-        # frmSensorAdd.grid(row=1, column=0, padx=5, pady=5)
-
-
-        # #sensor list
-        # frmSensorList = ttk.Frame(roomFrame)
-        # canvasSensorList = tk.Canvas(frmSensorList)
-        # scrollbarSensorList = ttk.Scrollbar(frmSensorList, orient="vertical", command=canvasSensorList.yview)
-        # scrollable_frame = ttk.Frame(canvasSensorList)
-        # canvasSensorList.create_window((0, 0), window=scrollable_frame, anchor="nw")
-        # canvasSensorList.configure(yscrollcommand=scrollbarSensorList.set)
-        
-        # canvasSensorList.bind("<Configure>", lambda e: canvasSensorList.configure(scrollregion = canvasSensorList.bbox("all") ))
-
-        # for i in range(100):
-        #     frmSensorInfo = ttk.Frame(scrollable_frame, width=50, height=10, relief=tk.GROOVE, borderwidth=5)
-
-        #     lblSensorName = ttk.Label(frmSensorInfo, text=f"Sensor id: {i}")
-        #     lblSensorValue = ttk.Label(frmSensorInfo, text="value: 0.0")
-            
-        #     frmSensorInfo.grid(row=i, column=0, sticky="nsew")
-
-        #     lblSensorName.grid(row=0, column=0)
-        #     lblSensorValue.grid(row=0, column=1)
-
-        # canvasSensorList.pack(side=tk.LEFT, fill="both", expand=True)
-        # scrollbarSensorList.pack(side=tk.RIGHT, fill="y")
-
-        # frmSensorList.grid(row=2, column=0, padx=5, pady=5)
-
-
-        # #3d vieuw placeholder
-        # frm3Dview = ttk.Frame(roomFrame)
-        # lbl3DTemp = ttk.Label(frm3Dview, font=('Helvetica', 30), text="3d placeholder")
-        # lbl3DTemp.pack(fill=tk.BOTH)
-        # frm3Dview.grid(row=2, column=1, padx=5, pady=5)
-
 
     def loadRooms(self): 
         self.controller.program.addRoomsFromNetwork()
