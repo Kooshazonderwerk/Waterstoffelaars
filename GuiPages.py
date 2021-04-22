90d2c935b47a91365f6fea9c4e7b0fe5e8d97dce
Pim van den Toorn <pimvdtoorn@hotmail.com>
2021-04-22 13:09:52 +0200

    entry buttons werkend room en sensor

--- GuiPages.py
+++ GuiPages.py
@@ -86,34 +86,55 @@ class EditRoomPage(tk.Frame):
         #Name
         frmEditRoomName = ttk.Frame(self)
         lblEditRoomName = ttk.Label(frmEditRoomName, text="Name: ")
-        entEditRoomName = ttk.Entry(frmEditRoomName)
+        self.entEditRoomName = ttk.Entry(frmEditRoomName)
         lblEditRoomName.grid(row=0, column=0, padx=5, pady=5)
-        entEditRoomName.grid(row=0, column=1, padx=5, pady=5)
+        self.entEditRoomName.grid(row=0, column=1, padx=5, pady=5)
         frmEditRoomName.grid(row=0, column=0, padx=5, pady=5, sticky="w")
         
         #dimensions
         frmEditDimensions = ttk.Frame(self)
         lblEditRoomWidth = ttk.Label(frmEditDimensions, text="Width: ")
-        entEditRoomWidth = ttk.Entry(frmEditDimensions)
+        self.entEditRoomWidth = ttk.Entry(frmEditDimensions)
         lblEditRoomWidth.grid(row=0, column=0, padx=5, pady=5,)
-        entEditRoomWidth.grid(row=0, column=1, padx=5, pady=5)
+        self.entEditRoomWidth.grid(row=0, column=1, padx=5, pady=5)
         lblEditRoomLength = ttk.Label(frmEditDimensions, text="Length: ")
-        entEditRoomLength = ttk.Entry(frmEditDimensions)
+        self.entEditRoomLength = ttk.Entry(frmEditDimensions)
         lblEditRoomLength.grid(row=1, column=0, padx=5, pady=5)
-        entEditRoomLength.grid(row=1, column=1, padx=5, pady=5)
+        self.entEditRoomLength.grid(row=1, column=1, padx=5, pady=5)
         lblEditRoomHeight = ttk.Label(frmEditDimensions, text="Height: ")
-        entEditRoomHeight = ttk.Entry(frmEditDimensions)
+        self.entEditRoomHeight = ttk.Entry(frmEditDimensions)
         lblEditRoomHeight.grid(row=2, column=0, padx=5, pady=5)
-        entEditRoomHeight.grid(row=2, column=1, padx=5, pady=5)
+        self.entEditRoomHeight.grid(row=2, column=1, padx=5, pady=5)
         frmEditDimensions.grid(row=1, column=0, padx=5, pady=5, sticky="w")
         #save and discard buttons
         frmEditSaveOrDiscard = ttk.Frame(self)
-        btnEditRoomSave = ttk.Button(frmEditSaveOrDiscard, text="Save and Exit", command=lambda: controller.show_frame(StartPage))
+        btnEditRoomSave = ttk.Button(frmEditSaveOrDiscard, text="Save and Exit", command=lambda: EditRoomPage.saveAndExit(self, controller))
         btnEditRoomSave.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-        btnEditRoomDiscard = ttk.Button(frmEditSaveOrDiscard, text="Discard and Exit", command=lambda: controller.show_frame(StartPage))
+        btnEditRoomDiscard = ttk.Button(frmEditSaveOrDiscard, text="Discard and Exit", command=lambda: EditRoomPage.discardAndExit(self, controller))
         btnEditRoomDiscard.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 
         frmEditSaveOrDiscard.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+
+    def saveAndExit(self, controller):
+        roomName = self.entEditRoomName.get()
+        roomX = self.entEditRoomWidth.get()
+        roomZ = self.entEditRoomLength.get()
+        roomY = self.entEditRoomHeight.get()
+        print(roomName)
+        #debugging^
+        self.entEditRoomName.delete(0, tk.END)
+        self.entEditRoomWidth.delete(0, tk.END)
+        self.entEditRoomLength.delete(0, tk.END)
+        self.entEditRoomHeight.delete(0, tk.END)
+        controller.show_frame(StartPage)
+
+    def discardAndExit(self, controller):
+        self.entEditRoomName.delete(0, tk.END)
+        self.entEditRoomWidth.delete(0, tk.END)
+        self.entEditRoomLength.delete(0, tk.END)
+        self.entEditRoomHeight.delete(0, tk.END)
+        controller.show_frame(StartPage)
+
 class EditSensorPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -121,23 +142,39 @@ class EditSensorPage(tk.Frame):
         #dimensions
         frmEditLocation = ttk.Frame(self)
         lblEditSensorX = ttk.Label(frmEditLocation, text="X: ")
-        entEditSensorX = ttk.Entry(frmEditLocation)
+        self.entEditSensorX = ttk.Entry(frmEditLocation)
         lblEditSensorX.grid(row=0, column=0, padx=5, pady=5,)
-        entEditSensorX.grid(row=0, column=1, padx=5, pady=5)
+        self.entEditSensorX.grid(row=0, column=1, padx=5, pady=5)
         lblEditSensorY = ttk.Label(frmEditLocation, text="Y: ")
-        entEditSensorY = ttk.Entry(frmEditLocation)
+        self.entEditSensorY = ttk.Entry(frmEditLocation)
         lblEditSensorY.grid(row=1, column=0, padx=5, pady=5)
-        entEditSensorY.grid(row=1, column=1, padx=5, pady=5)
+        self.entEditSensorY.grid(row=1, column=1, padx=5, pady=5)
         lblEditSensorZ = ttk.Label(frmEditLocation, text="Z: ")
-        entEditSensorZ = ttk.Entry(frmEditLocation)
+        self.entEditSensorZ = ttk.Entry(frmEditLocation)
         lblEditSensorZ.grid(row=2, column=0, padx=5, pady=5)
-        entEditSensorZ.grid(row=2, column=1, padx=5, pady=5)
+        self.entEditSensorZ.grid(row=2, column=1, padx=5, pady=5)
         frmEditLocation.grid(row=1, column=0, padx=5, pady=5, sticky="w")
         #save and discard buttons
         frmEditSensorSaveOrDiscard = ttk.Frame(self)
-        btnEditSensorSave = ttk.Button(frmEditSensorSaveOrDiscard, text="Save and Exit", command=lambda: controller.show_frame(StartPage))
+        btnEditSensorSave = ttk.Button(frmEditSensorSaveOrDiscard, text="Save and Exit", command=lambda: EditSensorPage.saveAndExit(self, controller))
         btnEditSensorSave.grid(row=0, column=0, padx=5, pady=5, sticky="w")
-        btnEditSensorDiscard = ttk.Button(frmEditSensorSaveOrDiscard, text="Discard and Exit", command=lambda: controller.show_frame(StartPage))
+        btnEditSensorDiscard = ttk.Button(frmEditSensorSaveOrDiscard, text="Discard and Exit", command=lambda: EditSensorPage.discardAndExit(self, controller))
         btnEditSensorDiscard.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 
         frmEditSensorSaveOrDiscard.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+    
+    def saveAndExit(self, controller):
+        sensorX = self.entEditSensorX.get()
+        sensorY = self.entEditSensorY.get()
+        sensorZ = self.entEditSensorZ.get()
+
+        self.entEditSensorX.delete(0, tk.END)
+        self.entEditSensorY.delete(0, tk.END)
+        self.entEditSensorZ.delete(0, tk.END)
+        controller.show_frame(StartPage)
+
+    def discardAndExit(self, controller):
+        self.entEditSensorX.delete(0, tk.END)
+        self.entEditSensorY.delete(0, tk.END)
+        self.entEditSensorZ.delete(0, tk.END)
+        controller.show_frame(StartPage)
